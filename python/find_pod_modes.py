3e809b1e164db5883e3b976d45b73dafcf761e05
Josh Williams <josh.williams@stfc.ac.uk>
2024-10-31 21:21:33 +0000

    add time data to pod_coefs, based on csv file in same dir

--- python/find_pod_modes.py
+++ python/find_pod_modes.py
@@ -13,6 +13,24 @@ from time import time
 def get_inputs():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
+        "--path-to-samples",
+        default=".",
+        type=str,
+        help="/path/to/samples",
+    )
+    parser.add_argument(
+        "--fieldname",
+        default="temperature",
+        type=str,
+        help="string for field name to lookup in vtk file",
+    )
+    parser.add_argument(
+        "--csvname",
+        default="*_out.csv",
+        type=str,
+        help="string for csv file to get timesteps",
+    )
+    parser.add_argument(
         "--num-samples",
         "-n",
         default=100000000,
@@ -32,15 +50,39 @@ def get_inputs():
         action="store_true",
         help="show debug checks",
     )
+    parser.add_argument(
+        "--nozero",
+        default=False,
+        action="store_true",
+        help="ignore 0th file",
+    )
     return parser.parse_args()
 
-def name_template(sample, time, block):
+def name_template(basedir, sample, time, block):
     """
     Returns string needed to read VTK data used for training
     """
-    return f"{sample}/vtk_data/{sample}/*/*_{time}_{block}_0.vtu"
+    return f"{basedir}/{sample}/vtk_data/{sample}/*/*_{time}_{block}_0.vtu"
 
-def read_data(sample_names, FIELDNAME="temperature"):
+def read_moose_csv(fname, nozero=True):
+    if "*" in fname:
+        fname = glob(fname)[0]
+    csv_arr = np.loadtxt(
+        fname, delimiter=",", usecols=[0], skiprows=1
+    )
+    warn("hard-coding skipping first row")
+    csv_arr = csv_arr[1:]
+    if nozero:
+        csv_arr = csv_arr[1:]
+    return csv_arr
+
+def read_data(
+    sample_names, 
+    FIELDNAME="temperature", 
+    csvname="*_out.csv", 
+    basedir=".", 
+    nozero=True
+):
     """
     Loop over all sample names and read vtk files from each timestep
     and each block.
@@ -74,10 +116,11 @@ def read_data(sample_names, FIELDNAME="temperature"):
     """
     field_snapshot_dict = dict.fromkeys(sample_names)
     dataset = []
+    time_dict = dict.fromkeys(sample_names)
 
     num_steps = 0 # all sims should have same number of steps at this point
     for s, sample in enumerate(sample_names):
-        fname_list = glob(name_template(sample, "*", "*"))
+        fname_list = glob(name_template(basedir, sample, "*", "*"))
         time_id_list = np.unique(
             [int(f.split("_")[-3]) for f in fname_list]
         ).tolist()
@@ -85,6 +128,8 @@ def read_data(sample_names, FIELDNAME="temperature"):
             [int(f.split("_")[-2]) for f in fname_list]
         ).tolist()
         time_id_list.sort()
+        if nozero:
+            time_id_list.pop(0)
         if s == 0:
             num_steps = len(time_id_list)
         else:
@@ -96,15 +141,16 @@ def read_data(sample_names, FIELDNAME="temperature"):
         # remove first timestep (initial cond), since variance = 0
         fname_list = fname_list[1:]
         field_snapshot_dict[sample] = []
+        time_name = f"{basedir}/{sample}/{csvname}"
+        time_dict[sample] = read_moose_csv(time_name, nozero)
         print("loading", sample)
 
         for i, time_t in enumerate(time_id_list):
-            # print(f"time = {time_t}")
             t0 = time()
             field_data_t = []
             mesh_list = []
             for block in block_id_list:
-                fname_template = name_template(sample, time_t, block)
+                fname_template = name_template(basedir, sample, time_t, block)
                 fname_list_block = glob(fname_template)
                 if len(fname_list_block) > 1:
                     warn(f"multiple files found {fname_list_block}")
@@ -121,15 +167,13 @@ def read_data(sample_names, FIELDNAME="temperature"):
             field_data_t = np.array(field_data_t).reshape(-1)
             field_snapshot_dict[sample].append(field_data_t)
             dataset.append(field_data_t)
-            # print(f"\t reading step took {time()-t0} s")
         field_snapshot_dict[sample] = np.array(field_snapshot_dict[sample])
     # just take the last instance of `mesh`
     # get mesh base
     mesh_base = []
-    num_pts_list = []
     for block in block_id_list:
         time_t = time_id_list[0]
-        fname_template = name_template(sample, time_t, block)
+        fname_template = name_template(basedir, sample, time_t, block)
         fname_list_block = glob(fname_template)
         if len(fname_list_block) > 1:
             warn(f"multiple files found {fname_list_block}")
@@ -137,15 +181,14 @@ def read_data(sample_names, FIELDNAME="temperature"):
             raise IndexError(f"{fname_template} not matching")
         fname = fname_list_block[0]
         mesh_base.append(v.load(fname))
-        num_pts_list.append(mesh_base[-1].pointdata[FIELDNAME].size)
 
     dataset = np.array(dataset)
     return (
         dataset,
-        num_pts_list,
         mesh_base,
         field_snapshot_dict,
         block_id_list,
+        time_dict,
     )
 
 
@@ -247,61 +290,6 @@ def get_dataset_coefs(
 
     return dataset_coefs_pertime
 
-
-def reconstruct_timeseries(
-    sample_params_pertime,
-    time_arr,
-    mean_dataset_columnvector,
-    pca_model_components,
-    NUM_MODES,
-    sam_obj,
-    true_data,
-):
-    """
-    Compute approximate/reconstructed field based on given coefficients/parameters
-
-    Parameters
-    ----------
-    sample_params_pertime : array_like
-        coefficients used to scale each mode
-    mean_dataset_columnvector : array_like
-        mean shape of the training data in a 1D array.
-    pca_model_components : array_like
-        eigenvectors of covariance matrix, obtain by PCA.
-    NUM_MODES : int
-        number of modes to use in POD expansion.
-    sam_obj : pyssam.SAM object
-        contains additional object information on the dataset
-    true_data : array_like
-        ground truth field values for computing error
-
-    Returns
-    -------
-    reconstructed_snapshot_arr : array_like
-        Array containing reconstructed field values for each time snapshot
-    """
-    t1 = time()
-    reconstructed_snapshot_arr = []
-    for i, (param_i, snapshot_i, time_t) in enumerate(
-        zip(sample_params_pertime, true_data, time_arr)
-    ):
-        model_params = param_i.copy()
-        reconstruction_scaled_i = sam_obj.morph_model(
-            mean_dataset_columnvector,
-            pca_model_components,
-            model_params,
-            num_modes=NUM_MODES,
-        )
-        reconstruction_i = reconstruction_scaled_i  # *scale_i) + mean_i
-        reconstructed_snapshot_arr.append(reconstruction_i)
-
-        diff = abs(reconstruction_i - snapshot_i)
-        error_list.append(diff)
-        time_list.append(time_t * DELTA_T)
-    t2 = time()
-    print(f"time taken {t2-t1}")
-    return np.array(reconstructed_snapshot_arr)
-
 def get_list_of_time_data(data_dict, col=0):
     """
     Helps rearrange data for post-processing
@@ -347,36 +335,64 @@ if __name__ == "__main__":
     POD_DIR = "pod_data/"
     makedirs(POD_DIR, exist_ok=True)
 
-    sample_names = glob("sample*")[: args.num_samples]
-    sample_names.sort()
+    sample_names = glob(f"{args.path_to_samples}/sample*")
+    sample_names = [s.split("/")[-1] for s in sample_names[: args.num_samples]]
     NUM_MODES = args.num_modes
 
     # read all vtk files. We have several samples, each sample has several timesteps
     # and each timestep may have several blocks (each with its own vtk file)
     # dataset is a np.ndarray of all data
-    # num_pts_list stores to size (int) of each block for reconstructing fields (not needed if only 1 block)
     # mesh_base is a vedo/vtk object containing mesh data
     # field_snapshot_dict has a dict entry for each sample. Each entry contains np.ndarray of time vs field data
     # block_id_list is a list of ints, with the IDs for each VTK block in the mesh
     (
         dataset,
-        num_pts_list,
         mesh_base,
         field_snapshot_dict,
         block_id_list,
-    ) = read_data(sample_names)
+        time_dict,
+    ) = read_data(
+        sample_names, 
+        FIELDNAME=args.fieldname, 
+        csvname=args.csvname, 
+        basedir=args.path_to_samples,
+        nozero=args.nozero
+    )
 
-    print(dataset.shape)
     # compute POD weights
     (
         mean_dataset_columnvector,
         pca_model_components,
         sam_obj,
     ) = setup_pod_model(dataset)
-    
+    # save POD weights
     np.savez(
         f"{POD_DIR}/pod_weights.npz",
         mean=mean_dataset_columnvector,
         pca_components=pca_model_components,
         pca_std=sam_obj.std,
     )
+    # compute and save POD coefficients
+    # dataset_coefs_pertime is list of arrays for each sample. 
+    #   Array shape is (n_time, n_coefs)
+    dataset_coefs_pertime = get_dataset_coefs(
+        sample_names,
+        field_snapshot_dict,
+        sam_obj,
+        mean_dataset_columnvector,
+        pca_model_components,
+        args.num_modes,
+    )
+    # write dataset_coefs_pertime. These are used later as target for regression model
+    for sample_i in sample_names:
+        # format header for columns in txt file
+        pod_coef_str = [f"c{c}" for c in range(args.num_modes)]
+        pod_coef_str = " ".join(pod_coef_str)
+        np.savetxt(
+            f"{POD_DIR}/pod_coefs_{sample_i}.txt", 
+            np.c_[
+                time_dict[sample_i],
+                dataset_coefs_pertime[sample_i]
+            ],
+            header=f"t {pod_coef_str}"
+        )
