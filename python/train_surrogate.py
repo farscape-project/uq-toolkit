33caa6aecc7545f20fde3db75c2df6e4c10ade69
Josh Williams <josh.williams@stfc.ac.uk>
2025-06-26 11:17:39 +0100

    add optional steady/unsteady i/o for surrogate modelling

--- python/train_surrogate.py
+++ python/train_surrogate.py
@@ -5,6 +5,7 @@ based on timestep and parameter (from uq_log*.json).
 import argparse
 import logging
 from glob import glob
+from os import makedirs
 from os.path import isfile
 
 import hjson as json
@@ -40,7 +41,7 @@ def get_inputs():
     )
     parser.add_argument(
         "--pod-dir",
-        default="pod_data/",
+        default=None,
         type=str,
         help="/path/to/dir containing pod_coefs_sample*.txt",
     )
@@ -62,6 +63,12 @@ def get_inputs():
         default=False,
         action="store_true",
         help="load model",
+    )
+    parser.add_argument(
+        "--steady-state",
+        default=False,
+        action="store_true",
+        help="data is not time-dependent",
     )
     return parser.parse_args()
 
@@ -159,35 +166,12 @@ def sklearn_gpr(x_train, y_train, x_test, y_test, hyperparams):
     logger.info(f"log_marginal_likelihood {gpr.log_marginal_likelihood_value_}")
     return gpr, score_train, score_test
 
-
-if __name__ == "__main__":
-    args = get_inputs()
-    logging.basicConfig(filename="train_surrogate.log", level=logging.INFO)
-
-    sample_names = [
-        s.split("/")[-1].split("_")[-1].split(".")[0]
-        for s in glob(f"{args.pod_dir}/pod_coefs_sample*")
-    ]
-
-    with open(args.uq_config) as f:
-        uq_config = json.load(f)
-
-    # read model hyperparams from file, or set to default
-    if isfile(args.surrogate_params):
-        with open(args.surrogate_params, "r") as f:
-            params = json.load(f)
-    else:
-        raise AssertionError(f"not found {args.surrogate_params}")
-    logger.info(f"surrogate hyperparams {params}")
-
-    app_log_list = find_uqlog_names(uq_config, results_dir=args.path_to_samples)
-    app_key_name_list = find_uqparam_names(app_log_list, "sample0")
-
+def load_data_timedependent(sample_names, params, app_log_list, app_key_name_list, pod_dir):
     dataset_coefs_pertime = dict.fromkeys(sample_names)
     x = []
     y = []
     for sample_i in sample_names:
-        coefs_data = np.loadtxt(f"{args.pod_dir}/pod_coefs_{sample_i}.txt", skiprows=1)
+        coefs_data = np.loadtxt(f"{pod_dir}/pod_coefs_{sample_i}.txt", skiprows=1)
         time_arr = coefs_data[:, 0]
         dataset_coefs_pertime[sample_i] = coefs_data[:, 1:]
         for t_index, time_i in enumerate(time_arr):
@@ -216,6 +200,77 @@ if __name__ == "__main__":
             x.append(x_i)
             y_i = [dataset_coefs_pertime[sample_i][t_index][1]]
             y.append(y_i)
+    return x, y
+
+def load_data_steady(sample_names, params, app_log_list, app_key_name_list, pod_dir):
+    dataset_coefs_pertime = dict.fromkeys(sample_names)
+    x = []
+    y = []
+    for sample_i in sample_names:
+        coefs_data = np.loadtxt(f"{pod_dir}/pod_coefs_{sample_i}.txt", skiprows=1)
+        dataset_coefs_pertime[sample_i] = coefs_data
+        x_i = []
+        y_i = []
+        # x_arr is [t, uq_0, uq_1, ..., uq_n]
+        # y is [pod_coef_0, ..., pod_coef_n]
+        for app_log, key_list in zip(app_log_list, app_key_name_list):
+            for key in key_list:
+                if isinstance(app_log[sample_i][key], list):
+                    x_i.extend(app_log[sample_i][key])
+                else:
+                    x_i.append(app_log[sample_i][key])
+
+                for norm_param_name, norm_value in params[
+                    "input-normalisations"
+                ].items():
+                    if norm_param_name in key:
+                        x_i[-1] = x_i[-1] / norm_value
+        x.append(x_i)
+        y_i = [dataset_coefs_pertime[sample_i][1]]
+        # y_i = dataset_coefs_pertime[sample_i]
+        y.append(y_i)
+    return x, y
+
+if __name__ == "__main__":
+    args = get_inputs()
+    logging.basicConfig(filename="train_surrogate.log", level=logging.INFO)
+    
+    # by default, POD data will be in the results dir. 
+    # But in the nextflow pipeline, we will write to a temporary cache directory.
+    if args.pod_dir is None:
+        POD_DIR = f"{args.path_to_samples}/pod_data"
+    else:
+        POD_DIR = args.pod_dir
+    
+    # make plotting directory
+    makedirs(f"{args.path_to_samples}/plots", exist_ok=True)
+
+    # read all existing sample names
+    with open(f"{args.path_to_samples}/complete_samples.txt", "r") as f:
+        file_lines = f.read()
+        sample_names = file_lines.strip().split("\n")
+
+    with open(args.uq_config) as f:
+        uq_config = json.load(f)
+
+    # read model hyperparams from file, or set to default
+    if isfile(args.surrogate_params):
+        with open(args.surrogate_params, "r") as f:
+            params = json.load(f)
+    else:
+        raise AssertionError(f"not found {args.surrogate_params}")
+    logger.info(f"surrogate hyperparams {params}")
+
+    app_log_list = find_uqlog_names(uq_config, results_dir=args.path_to_samples)
+    app_key_name_list = find_uqparam_names(app_log_list, "sample0")
+
+    dataset_coefs_pertime = dict.fromkeys(sample_names)
+    if args.steady_state:
+        data_func = load_data_steady
+    else:
+        data_func = load_data_timedependent
+        
+    x, y = data_func(sample_names, params, app_log_list, app_key_name_list, POD_DIR)
 
     # randomly shuffle and then split data into train/test
     TRAIN_FRACTION = 0.8
